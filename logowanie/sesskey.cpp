850387414f6e8df8e726ff01048c6155a82212dd
s17kf <S.Sokol.1@stud.elka.pw.edu.pl>
2018-06-02 22:34:32 +0200

    login sequence working

---
+++ logowanie/sesskey.cpp
@@ -0,0 +1,97 @@
+#include <openssl/err.h>
+#include <stdexcept>
+#include <cstring>
+
+#include "sesskey.h"
+#include "log.h"
+#include "RNG.h"
+
+//extern RNG rng;
+
+Sesskey::Sesskey() {
+    RNG rng;
+    if (rng.generate(key, (size_t) 16) == -1) {
+        log(3, "Unable to create sesskey.");
+    }
+    /*if (!(ctx = EVP_CIPHER_CTX_new()))
+        log(3, "Unable to create OPENSSL context.\n");*/
+    ctx = EVP_CIPHER_CTX_new();
+}
+Sesskey::Sesskey(const KEY &keyPck, const Privkey &privkey) {
+    /*if (!(ctx = EVP_CIPHER_CTX_new()))
+        log(3, "Unable to create OPENSSL context.\n");*/
+    ctx = EVP_CIPHER_CTX_new();
+    if (privkey.decrypt(keyPck.getKeyBuf(), 256, key) == -1){
+        log(3, "Unable to decrypt session key");
+    }
+//	memcpy(key, keyPck.getKeyBuf(), 16);
+}
+
+Sesskey::~Sesskey() {
+    EVP_CIPHER_CTX_free(ctx);
+}
+
+// This function is called only for messages that needs encryption
+int Sesskey::encrypt(unsigned char *dest, const unsigned char *src, size_t src_size) const{
+    int len, ciphertext_len;
+    unsigned char iv[16];
+    RNG rng;
+    ciphertext_len = 0;
+    if (rng.generate(iv, (size_t) 16) == -1) {
+        log(3, "Unable to create IV");
+        return -1;
+    }
+
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv)) {
+        log(3, "Unable to init EVP encryption.");
+        return -1;
+    }
+    //while (ciphertext_len < src_size) {
+    if (1 != EVP_EncryptUpdate(ctx, dest, &len, src, src_size)){
+        log(3, "Unable to encrypt.");
+        return -1;
+    }
+    ciphertext_len += len;
+    //}
+    if(1 != EVP_EncryptFinal_ex(ctx, dest + len, &len)) {
+        log(3, "Unable to finalize EVP encryption.");
+        return -1;
+    }
+    ciphertext_len += len;
+    memcpy(dest + ciphertext_len, iv, 16);
+    ciphertext_len += 16;
+
+    return ciphertext_len;
+}
+
+// This function is called only for messages that needs decryption
+int Sesskey::decrypt(unsigned char *dest, const unsigned char *src, size_t src_size) const{
+    const unsigned char *iv;
+    unsigned char plain_size;
+    int len;
+    int msg_buf_size;
+
+    msg_buf_size = src_size - 16;// -16 for iv
+
+    iv = &src[src_size - 16];// -16 for iv, -1 for zero-based counting
+    if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv)) {
+        log(3, "Unable to init EVP decryption.");
+        return -1;
+    }
+
+    if (1 != EVP_DecryptUpdate(ctx, dest, &len, src, msg_buf_size)) {
+        log(3, "Unable to decrypt.");
+        return -1;
+    }
+
+    if (1 != EVP_DecryptFinal_ex(ctx, dest + len, &len)) {
+        ERR_print_errors_fp(stderr);
+        log(3, "Unable to finalize EVP decryption.");
+        return -1;
+    }
+
+    return 0;
+}
+const unsigned char* Sesskey::getKeyBuf() const {
+    return key;
+}
---
+++ logowanie/sesskey.h
@@ -0,0 +1,23 @@
+#ifndef TIN_SESSKEY
+#define TIN_SESSKEY
+
+#include <openssl/evp.h>
+#include "packet.h"
+#include "privkey.h"
+
+class KEY;
+
+class Sesskey {
+private:
+    unsigned char key[16];
+    EVP_CIPHER_CTX *ctx;
+public:
+    Sesskey();
+    Sesskey(const KEY &key, const Privkey &privkey);
+    ~Sesskey();
+    /* Both encode and decode messages assumes that encrypted message has IV in the end */
+    int encrypt(unsigned char *dest, const unsigned char *src, size_t src_size) const;
+    int decrypt(unsigned char *dest, const unsigned char *src, size_t src_size) const;
+    const unsigned char *getKeyBuf() const;
+};
+#endif //TIN_SESSKEY
